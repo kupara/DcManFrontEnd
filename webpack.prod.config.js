b9ea93ff54becb0a26045dfa8ce0461febb9fdab
andela-ekupara <edwin.kupara@andela.com>
2016-04-20 18:06:03 +0300

    [#117955889] Heroku deployment fix

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,38 +1,49 @@
-const path = require('path')
-const webpack = require('webpack')
-const appPath = path.resolve(__dirname, 'app', 'app.js');
-module.exports = {
-  devtool: 'source-map',
+(() => {
+  'use strict';
 
-  entry: {
-    main: [
-      appPath
-    ]
-  },
+  var webpack = require('webpack');
+  var path = require('path');
+  let ExtractTextPlugin = require('extract-text-webpack-plugin');
+  let buildPath = path.resolve(__dirname, 'public', 'build');
+  var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+  let pathToReact = path.resolve(nodeModulesPath, 'react/dist/react.min.js');
+  let pathToReactDOM = path.resolve(
+    nodeModulesPath,
+    'react-dom/dist/react-dom.min.js');
+  let mainPath = path.resolve(__dirname, 'app', 'app.js');
 
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
-  },
-
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
+  var config = {
+    entry: [mainPath],
+    resolve: {
+      // node_modules: ["web_modules", "node_modules"]  (Default Settings)
+      alias: {
+        'react': pathToReact,
+        'react-dom': pathToReactDOM
       }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
-
-  module: {
-    loaders: [{
+    },
+    target: 'web',
+    // Render source-map file for final build
+    devtool: 'source-map',
+    // output config
+    output: {
+      path: buildPath, // Path of output file
+      filename: 'bundle.js', // Name of output file
+      publicPath: '/build/'
+    },
+    plugins: [
+      new ExtractTextPlugin('styles.css'),
+      // Minify the bundle
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          // supresses warnings, usually from module minification
+          warnings: false
+        }
+      }),
+      // Allows error warnings but does not stop compiling. Will remove when eslint is added
+      new webpack.NoErrorsPlugin()
+    ],
+    module: {
+      loaders: [{
       test: /\.jsx?$/,
       include: path.join(__dirname, 'app'),
       loader: 'babel-loader',
@@ -41,7 +52,7 @@ module.exports = {
       {
         presets:['react', 'es2015']
       }
-    }, {
+      }, {
         test: /(\.scss|\.css)$/,
         loaders: [
           require.resolve('style-loader'),
@@ -49,11 +60,15 @@ module.exports = {
           require.resolve('sass-loader') + '?sourceMap'
         ]
       },
-    {
-      test: /\.jpg?$/,
-      include: path.join(__dirname, 'app'),
-      exclude: [path.resolve(__dirname, 'node_modules')],
-      loader: 'url-loader'
-    }]
-  }
-}
+      {
+        test: /\.jpg?$/,
+        include: path.join(__dirname, 'app'),
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'url-loader'
+      }],
+      noParse: [pathToReact]
+    }
+  };
+
+  module.exports = config;
+})();
